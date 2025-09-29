8aed5a7524ae24ede74bab5b76d6511784dd52e3
gbmatheus <gabrielmatheus2503@gmail.com>
2025-09-29 18:12:25 -0300

    test: unit test to transaction and category

---
+++ tests/NaReta.Domain.Test/Entities/CategoryTest.cs
@@ -0,0 +1,28 @@
+﻿using NaReta.Domain.Entities;
+using Shouldly;
+
+namespace NaReta.Domain.Test.Entities
+{
+    public class CategoryTest
+    {
+        [Fact]
+        public void Contructor_ValidParameters_CreateCategory()
+        {
+            string title = "Alimentação";
+            var category = new Category(title);
+            category.ShouldNotBeNull();
+            category.Title.ShouldBe(title);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("      ")]
+        public void Contructor_EmptyOrNullType_ThrowArgumentException(string title)
+        {
+            var act = () => new Category(title);
+
+            act.ShouldThrow<ArgumentException>(ResourceErrorMessages.TITLE_EMPTY_OR_NULL);
+        }
+    }
+}
--- tests/NaReta.Domain.Test/Entities/TransactionTest.cs
+++ tests/NaReta.Domain.Test/Entities/TransactionTest.cs
@@ -1,15 +1,66 @@
-﻿namespace NaReta.Domain.Test.Entities
+﻿using NaReta.Domain.Entities;
+using NaReta.Domain.Enums;
+using Shouldly;
+
+namespace NaReta.Domain.Test.Entities
 {
     public class TransactionTest
     {
         [Fact]
-        public void Create_Transaction_Instance_Success()
+        public void Constructor_ValidParameters_CreateTransaction()
         {
             // Arrange
-            var transcation = new Transaction(1, "despesa", 100.02, new DateTime(2025, 1, 1), "despesa 1");
+            TransactionTypeEnum TYPE = TransactionTypeEnum.Expense;
+            const decimal AMOUNT = 100.02m;
+            DateTime DATE = new DateTime(2025, 1, 1);
+            const string DESCRIPTION = "despesa 1";
+            var category = new Category("Alimentação");
+
             // Act
+            var transcation = new Transaction(TYPE, AMOUNT, DATE, category, DESCRIPTION);
 
             // Assert
+            transcation.ShouldNotBeNull();
+            transcation.Type.ShouldBe(TYPE);
+            transcation.Amount.ShouldBe(AMOUNT);
+            transcation.Date.ShouldBe(DATE);
+            transcation.Description.ShouldBe(DESCRIPTION);
         }
+
+        [Fact]
+        public void Constructor_EmptyOrNullType_ThrowArgumentException()
+        {
+            // Arrange
+            const decimal AMOUNT = 100.02m;
+            DateTime DATE = new DateTime(2025, 1, 1);
+            const string DESCRIPTION = "despesa 1";
+            var category = new Category("Alimentação");
+
+            // Act
+            var act = () => new Transaction((TransactionTypeEnum) 3, AMOUNT, DATE, category, DESCRIPTION);
+
+            // Assert
+            act.ShouldThrow<ArgumentException>(ResourceErrorMessages.TYPE_INVALID);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-19232)]
+        public void Constructor_AmountEqualOrLessThanZero_ThrowArgumentException(Decimal amount)
+        {
+            // Arrange
+            TransactionTypeEnum TYPE = TransactionTypeEnum.Expense;
+            DateTime DATE = new DateTime(2025, 1, 1);
+            const string DESCRIPTION = "despesa 1";
+            var category = new Category("Alimentação");
+
+            // Act
+            var act = () => new Transaction(TYPE, amount, DATE, category, DESCRIPTION);
+
+            // Assert
+            act.ShouldThrow<ArgumentException>(ResourceErrorMessages.AMOUNT_EQUAL_OR_LESS_ZERO);
+        }
+
     }
 }
