b9c148e59bbfc45ea3d3a5434d5482078160639a
gbmatheus <gabrielmatheus2503@gmail.com>
2025-09-29 18:18:13 -0300

    chore: create api project

---
+++ src/NaReta.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NaReta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+    }
+}
