b4acf580b96fb4a2a329011fe701d5c5385604d9
gbmatheus <gabrielmatheus2503@gmail.com>
2025-11-14 10:47:17 -0300

    feat: configure dependency injection setup

---
+++ src/NaReta.Application/DependencyInjectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NaReta.Application.UseCases.Account.Create;
+using NaReta.Application.UseCases.Account.Get;
+using NaReta.Application.UseCases.Category.Create;
+using NaReta.Application.UseCases.Category.List;
+using NaReta.Application.UseCases.Category.Update;
+using NaReta.Application.UseCases.Transaction.Create;
+using NaReta.Application.UseCases.Transaction.Delete;
+using NaReta.Application.UseCases.Transaction.List;
+using NaReta.Application.UseCases.Transaction.Update;
+using NaReta.Application.UseCases.Transactions.Create;
+
+namespace NaReta.Application;
+public static class DependencyInjectionExtensions
+{
+    public static void AddApplication(this IServiceCollection service)
+    {
+        AddUseCase(service);
+    }
+
+    private static void AddUseCase(IServiceCollection service)
+    {
+        service.AddScoped<ICreateCategoryUseCase, CreateCategoryUseCase>();
+        service.AddScoped<IListCategoryUseCase, ListCategoryUseCase>();
+        service.AddScoped<IUpdateCategoryUseCase, UpdateCategoryUseCase>();
+
+        service.AddScoped<ICreateAccountUseCase, CreateAccountUseCase>();
+        service.AddScoped<IListAccountUseCase, ListAccountUseCase>();
+        service.AddScoped<IGetAccountUseCase, GetAccountUseCase>();
+
+        service.AddScoped<ICreateTransactionUseCase, CreateTransactionUseCase>();
+        service.AddScoped<IListTransactionUseCase, ListTransactionUseCase>();
+        service.AddScoped<IUpdateTrasanctionUseCase, UpdateTrasanctionUseCase>();
+        service.AddScoped<IDeleteTransactionUseCase, DeleteTransactionUseCase>();
+    }
+
+}
