d66d0568363a73482fc96e78d59f6fe9a3c1c55c
gbmatheus <gabrielmatheus2503@gmail.com>
2025-11-15 00:43:46 -0300

    feat: add transction into result to get account use case

--- src/NaReta.Application/UseCases/Account/Get/GetAccountUseCase.cs
+++ src/NaReta.Application/UseCases/Account/Get/GetAccountUseCase.cs
@@ -1,17 +1,22 @@
 ﻿
 using NaReta.Application.UseCases.Account._Common;
-using NaReta.Domain;
+using NaReta.Common;
 using NaReta.Domain.Repositories.Accounts;
+using NaReta.Domain.Repositories.Transactions;
 
 namespace NaReta.Application.UseCases.Account.Get;
+
 internal class GetAccountUseCase : IGetAccountUseCase
 {
     private readonly IAccountReadOnlyRepository _repository;
+    private readonly ITransactionReadOnlyRepository _transactionRepository;
 
     public GetAccountUseCase(
-        IAccountReadOnlyRepository repository)
+        IAccountReadOnlyRepository repository,
+        ITransactionReadOnlyRepository transactionRepository)
     {
         _repository = repository;
+        _transactionRepository = transactionRepository;
     }
 
     public async Task<OutputAccount> ExecuteAsync(int id)
@@ -22,9 +27,28 @@ internal class GetAccountUseCase : IGetAccountUseCase
             // [TODO] NotFound
             throw new Exception(ResourceErrorMessages.ACCOUNT_NOT_FOUND);
 
+        // Error - Balanço com o ultimo valor da transação
+        //account.CalculateBalance();
+
+        var outputTransaction = new List<OutputTransactionIntoAccount>();
+        account.Transactions.ForEach(transaction =>
+        {
+            outputTransaction.Add(new OutputTransactionIntoAccount
+            {
+                Id = transaction.Id,
+                Description = transaction.Description,
+                Amount = transaction.Amount,
+                Date = transaction.Date,
+                Type = transaction.Type.ToString(),
+                Category = transaction.Category.Name
+            });
+        });
+
         return new OutputAccount
         {
             Name = account.Name,
+            Balance = account.Balance,
+            Transactions = outputTransaction
         };
 
     }
--- src/NaReta.Application/UseCases/Account/_Common/OutputAccount.cs
+++ src/NaReta.Application/UseCases/Account/_Common/OutputAccount.cs
@@ -1,5 +1,23 @@
-﻿namespace NaReta.Application.UseCases.Account._Common;
+﻿using System.Text.Json.Serialization;
+
+namespace NaReta.Application.UseCases.Account._Common;
 public class OutputAccount
 {
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public List<OutputTransactionIntoAccount>? Transactions { get; set; }
+}
+
+public class OutputTransactionIntoAccount
+{
+    public int Id { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
 }
