fb676e6340132614c909301c4172fbd692e8507c
gbmatheus <gabrielmatheus2503@gmail.com>
2025-11-14 21:14:56 -0300

    feat: add domain exception and exception base

---
+++ src/NaReta.Common/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Net;
+
+namespace NaReta.Common.Exceptions;
+
+public class DomainException : NaRetaExceptionBase
+{
+    public DomainException(string message) : base(message) { }
+
+    public override int StatusCode => (int)HttpStatusCode.UnprocessableEntity;
+
+    public override List<string> GetErrors() => new List<string> { Message };
+}
---
+++ src/NaReta.Common/Exceptions/NaRetaExceptionBase.cs
@@ -0,0 +1,9 @@
+﻿namespace NaReta.Common.Exceptions;
+
+public abstract class NaRetaExceptionBase : System.Exception
+{
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
+
+    public NaRetaExceptionBase(string message) : base(message) { }
+}
