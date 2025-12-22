3e520587cb7f6da95b9a35b933ed5c038fa1ef6a
gbmatheus <gabrielmatheus2503@gmail.com>
2025-12-22 19:30:45 -0300

    refactor: update IUnitOfWork to expose repository properties and use async commit

--- src/NaReta.Domain/Repositories/IUnitOfWork.cs
+++ src/NaReta.Domain/Repositories/IUnitOfWork.cs
@@ -1,7 +1,14 @@
-﻿namespace NaReta.Domain.Repositories
+﻿using NaReta.Domain.Repositories.Accounts;
+using NaReta.Domain.Repositories.Categories;
+using NaReta.Domain.Repositories.Transactions;
+
+namespace NaReta.Domain.Repositories
 {
     public interface IUnitOfWork
     {
-        Task Commit();
+        IAccountWriteOnlyRepository AccountWriteOnlyRepository { get; }
+        ICategoryWriteOnlyRepository CategoryWriteOnlyRepository { get; }
+        ITransactionWriteOnlyRepository TransactionWriteOnlyRepository { get; }
+        Task CommitAsync();
     }
 }
